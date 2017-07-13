912139707cf8a8b0eb061058d3420d3a5697e4df
Justice Yen <justice.yen@gmail.com>
2017-07-12 23:46:33 -0700

    add script when no posts are found

--- main.js
+++ main.js
@@ -1,34 +1,39 @@
 $(document).ready( () => {
   let postData =[];
   let commentData = [];
+  let postTitle = $("#postTitle")[0];
+  let commentTitle = $("#commentTitle")[0];
   $("#submit").click( () => {
     let username = $("#username")[0].value;
-    $.get(`https://www.reddit.com/user/${username}/submitted.json`,
-    (data) => {
-      let posts = data.data.children;
-      posts = posts.slice(0, Math.min(posts.length, 25));
-      sortedPosts = posts.sort( (a,b) => {
-        if(a.data.score < b.data.score) {
-          return 1;
-        } else if(a.data.score > b.data.score) {
-          return -1;
-        } else {
-          return 0;
+    $.ajax({
+      url: `https://www.reddit.com/user/${username}/submitted.json`,
+      success: (data) => {
+        let posts = data.data.children;
+        posts = posts.slice(0, Math.min(posts.length, 25));
+        sortedPosts = posts.sort( (a,b) => {
+          if(a.data.score < b.data.score) {
+            return 1;
+          } else if(a.data.score > b.data.score) {
+            return -1;
+          } else {
+            return 0;
+          }
+        });
+        for(let i=0; i<sortedPosts.length; i++) {
+          let post = posts[i];
+          let postInfo= document.createElement("li");
+          let postTitle = document.createElement("h3");
+          postTitle.innerHTML = `${post.data.title}`;
+          let postScore = document.createElement("p");
+          postScore.innerHTML = `Score: ${post.data.score}`;
+          let postLink = document.createElement("a");
+          postLink.innerHTML = "Link";
+          postLink.href = `${post.data.url}`;
+          postInfo.append(postTitle, postScore, postLink);
+          $("#postList")[0].append(postInfo);
         }
-      });
-      for(let i=0; i<sortedPosts.length; i++) {
-        let post = posts[i];
-        let postInfo= document.createElement("li");
-        let postTitle = document.createElement("h3");
-        postTitle.innerHTML = `${post.data.title}`;
-        let postScore = document.createElement("p");
-        postScore.innerHTML = `Score: ${post.data.score}`;
-        let postLink = document.createElement("a");
-        postLink.innerHTML = "Link";
-        postLink.href = `${post.data.url}`;
-        postInfo.append(postTitle, postScore, postLink);
-        $("#postList")[0].append(postInfo);
-      }
+      },
+      error: () => {postTitle.innerHTML = `No posts by "${username}" found!`}
     });
     $.get(`https://www.reddit.com/user/${username}/comments.json`,
     (data) => {
